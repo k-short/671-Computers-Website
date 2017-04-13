9457a59374570a1f9e1ab38d1546c2b4d02b6bba
Sims Osborne <shosborn@users.noreply.github.com>
2017-04-13 16:26:29 -0400

    php file for admin to show all default configs
    Has the same issue with the shop results of the result table being misaligned.

---
+++ showAllDefaults.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Show Default Systems</title>
+<style>
+table, th, td {
+     border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+<h1>Default Systems</h1>
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Project";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+$sql = "Select * From DefaultFullInfo";   
+//$result = $conn->query($sql);
+$result=mysqli_query($conn, $sql);
+
+//if ($result->num_rows > 0) {
+//if ($result!=false){
+if (mysqli_num_rows($result)>0){
+	echo"<table><tr> <th>Item Number</th> <th>Style</th> <th>Weight<th/> <th>Measurement</th> <th>Processor</th> <th>Memory</th> <th>Storage</th> <th>Storage Type</th> <th>Price</th> </tr>";  
+     // output data of each row
+	while($row = $result->fetch_assoc()) {
+	$price = $row["chPrice"] + $row["storPrice"] + $row["memPrice"];
+	//Not sure if embedding chPrice+storPrice+memPrice here is legal	
+         echo "<tr><td>" . $row["defaultNo"]. "</td><td>" . $row["style"]. "</td><td>" . $row["weight"]. 
+		 "</td><td>" . $row["measures"]. "</td><td>" . $row["processorSpeed"]. "</td> <td>".$row["memSize"].
+		 "</td> <td>".$row["storSize"]. "</td> <td>".$row["storType"]. "</td> <td>".$price."</td></tr>";
+     }
+     echo "</table>";
+} else {
+     echo "0 results";
+}
+  
+$conn->close();
+
+?>  
+
+
+
+</body>
+</html>
