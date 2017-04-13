5ae05d2982f82369ae2b5cd8618503803b5a1c05
Sims Osborne <shosborn@users.noreply.github.com>
2017-04-13 16:25:11 -0400

    php file to allow admin to create new default
    Adds new default to database, or alerts admin if an identical configuration already exists.  itemNumber for new default is # of items in list +1; if anyone feels ambitious and wants to change that, feel free.  Assumes that the chassis, memory and storage options selected are actually available.

---
+++ newDefault.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Add new Default System</title>
+<style>
+table, th, td {
+     border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+<h1>Search Results</h1>
+
+<?php
+
+function addDefault($chWanted, $memoryWanted, $speedWanted, $storTypeWanted, $storWanted){ 
+
+
+	//hard coded values for testing
+	/*
+$chWanted="L700";
+$storTypeWanted="SSD";
+$storWanted=1600;
+$memoryWanted=4;
+	 */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Project";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+
+//check if system already exists
+
+$sql = "Select *
+	From DefaultSystem 
+	Where chNumber='".$chWanted."'".
+	" AND storType='".$storTypeWanted."'".
+	" And storSize>=".$storWanted.
+	" AND memSize>=".$memoryWanted;   
+//$result = $conn->query($sql);
+$result=mysqli_query($conn, $sql);
+
+
+if (mysqli_num_rows($result)>0){
+	echo "A default system already exists with that configuration!";
+} 
+else {
+	//create item number for new system; make sure it doesn't conflict
+	//add new system
+	$sqlCount="Select * from DefaultSystem";
+	$resultCount=mysqli_query($conn, $sqlCount);
+	$itemNumber=(mysqli_num_rows($resultCount))+1;
+
+	$sqlAdd="insert into DefaultSystem values('".$itemNumber."', '".$chWanted."', '".$storTypeWanted."', ".$storWanted.", ".$memoryWanted.")";
+	$result=mysqli_query($conn, $sqlAdd);
+     echo "New system added with item number " . $itemNumber.".";
+}
+ 
+$conn->close();
+}
+?>  
+
+
+
+</body>
+</html>
